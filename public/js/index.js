dafbac0e6378df8f511a4a927998e6d9a76d55bd
INDRAJIT SINGH <indrajeet654@gmail.com>
2019-09-09 16:26:46 +0530

    code

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+
 var socket = io();
 
 socket.on('connect', () => {
@@ -11,16 +12,18 @@ socket.on('newEmail', (data) => {
 })
 
 socket.on('newMessage', (newMessage) => {
+    var formattedTime=moment(newMessage.createdat).format('h:mm a')
     console.log("newMessage", newMessage);
     var li = jQuery('<li></li>');
-    li.text(`${newMessage.from}:${newMessage.text}`);
+    li.text(`${newMessage.from} ${formattedTime}:${newMessage.text}`);
     jQuery('#messages').append(li)
 })
 
 socket.on('newLocationMessage', (message) => {
+    var formattedTime = moment(message.createdat).format('h:mm a')
     var li = jQuery('<li></li>')
     var a = jQuery('<a target="_blank">My current location</a>')
-    li.text(`${message.from}:`)
+    li.text(`${message.from} ${formattedTime}:`)
     a.attr('href', message.url)
     li.append(a);
     jQuery('#messages').append(li)
